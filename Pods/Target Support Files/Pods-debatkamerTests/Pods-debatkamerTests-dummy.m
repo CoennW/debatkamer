72e081379ae5168a12f0fa83d12b97534e4df07d
Ruud Slagers <r.slagers@student.fontys.nl>
2017-05-19 14:02:07 +0200

    Email-firebase-compleet

---
+++ Pods/Target Support Files/Pods-debatkamerTests/Pods-debatkamerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_debatkamerTests : NSObject
+@end
+@implementation PodsDummy_Pods_debatkamerTests
+@end
--- debatkamer/LoginVC.swift
+++ debatkamer/LoginVC.swift
@@ -7,21 +7,40 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginVC: UIViewController {
-
+    @IBOutlet weak var pwdField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-
-
+    @IBAction func signInTapped(_ sender: Any) {
+        if let email = emailField.text, let pwd = pwdField.text {
+            Auth.auth().signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil {
+                    print("ADMIN: Email gebruiker maakt connectie met Firebase")
+                } else {
+                    Auth.auth().createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("ADMIN: Kan geen connectie maken met Firebase")
+                        } else {
+                            print("ADMIN: Succesvol verbinding gemaakt met Firebase")
+                        }
+                    })
+                }
+            })
+        }
+    }
 }

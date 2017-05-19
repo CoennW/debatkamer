f8e8de1ac562e7405c98af92dba9749481193fcc
Ruud Slagers <r.slagers@student.fontys.nl>
2017-05-19 11:02:02 +0200

    Eerste commitment

---
+++ debatkamer/LoginVC.swift
@@ -0,0 +1,27 @@
+//
+//  LoginVC.swift
+//  debatkamer
+//
+//  Created by Ruud Slagers on 12/05/2017.
+//  Copyright © 2017 Ruud Slagers. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}

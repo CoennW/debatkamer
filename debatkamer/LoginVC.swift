46ad7bcede72e0448affc259a6d67d3635cbb477
Ruud Slagers <r.slagers@student.fontys.nl>
2017-05-19 15:22:50 +0200

    keychain

--- debatkamer/LoginVC.swift
+++ debatkamer/LoginVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import SwiftKeychainWrapper
 
 class LoginVC: UIViewController {
     @IBOutlet weak var pwdField: UITextField!
@@ -15,32 +16,44 @@ class LoginVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let _ = KeychainWrapper.standard.string(forKey: KEY_UID) {
+            print("ID gevonden in keychain")
+            performSegue(withIdentifier: "GoToStemmen", sender: nil)
+        }
     }
     
     
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     @IBAction func signInTapped(_ sender: Any) {
         if let email = emailField.text, let pwd = pwdField.text {
             Auth.auth().signIn(withEmail: email, password: pwd, completion: { (user, error) in
                 if error == nil {
                     print("ADMIN: Email gebruiker maakt connectie met Firebase")
+                    if let user = user {
+                        self.completeSignIn(id: user.uid)
+                    }
                 } else {
                     Auth.auth().createUser(withEmail: email, password: pwd, completion: { (user, error) in
                         if error != nil {
                             print("ADMIN: Kan geen connectie maken met Firebase")
                         } else {
                             print("ADMIN: Succesvol verbinding gemaakt met Firebase")
+                            if let user = user {
+                                self.completeSignIn(id: user.uid)
+                            }
                         }
                     })
                 }
             })
         }
     }
+    func completeSignIn(id: String) {
+        let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
+        print("ADMIN: Data opgeslagen aan de Keychain \(keychainResult)")
+        performSegue(withIdentifier: "GoToStemmen", sender: nil)
+    }
+    
 }
---
+++ debatkamer/variable.swift
@@ -0,0 +1,11 @@
+//
+//  variable.swift
+//  debatkamer
+//
+//  Created by Ruud Slagers on 19/05/2017.
+//  Copyright © 2017 Ruud Slagers. All rights reserved.
+//
+
+import UIKit
+
+let KEY_UID = "uid"
